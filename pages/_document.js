dff5b2df8613ea4d879bc1656e4852543f39e25e
nilevia <otniel@alterra.id>
2020-09-09 17:03:50 +0700

    document

---
+++ pages/_document.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+
+const APP_NAME = "Atur Tagihan";
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <html lang="en">
+            <Head>
+                <meta name="application-name" content={APP_NAME} />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta
+                    name="apple-mobile-web-app-status-bar-style"
+                    content="default"
+                />
+                <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+                {/*<meta name="description" content={APP_DESCRIPTION} />*/}
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="theme-color" content="#FFFFFF" />
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+                />
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/icons/apple-touch-icon.png"
+                />
+
+                {/* PWA primary color */}
+                {/*<meta name="theme-color" content={theme.palette.primary.main} />*/}
+                {/* <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" /> */}
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+            </html>
+        );
+    }
+}
+
+MyDocument.getInitialProps = async ctx => {
+    // Resolution order
+    //
+    // On the server:
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. document.getInitialProps
+    // 4. app.render
+    // 5. page.render
+    // 6. document.render
+    //
+    // On the server with error:
+    // 1. document.getInitialProps
+    // 2. app.render
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the client
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. app.render
+    // 4. page.render
+
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: App => props => sheets.collect(<App {...props} />)
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+        ...initialProps,
+        // Styles fragment is rendered after the app and page rendering finish.
+        styles: [
+            ...React.Children.toArray(initialProps.styles),
+            sheets.getStyleElement()
+        ]
+    };
+};
