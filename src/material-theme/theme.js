e095dacbc8f6babcc83aba3a52abe8501a7a1823
nilevia <otniel@alterra.id>
2020-09-09 17:11:15 +0700

    custom style

---
+++ src/material-theme/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#4e7ff7',
+            hover: '#023989',
+        },
+    },
+});
+export default theme;
