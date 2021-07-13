f8db6d080a4cf35db413cc8e334e57bdcf4da65d
Nadiah <042574432@ecampus.ut.ac.id>
2021-07-13 20:14:12 +0700

    add antd module

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 export default class Home extends Component {
   render() {
     return (
         <div>
           <p>Otniel Victory Febe Anwar</p>
           <p>Use this page as index or home of your website</p>
+            <Button>Tes</Button>
         </div>
     );
   }
