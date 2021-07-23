838009514e1043e8d54a839cda0172464dcc2c2b
Nadiah <042574432@ecampus.ut.ac.id>
2021-07-23 12:52:17 +0700

    add css cardComponent

--- components/global-component/Card.js
+++ components/global-component/Card.js
@@ -4,13 +4,17 @@ import { Card, Rate } from 'antd';
 export default function CardComponent(props) {
     const { image, title, category, description, rating, allowHalfRate, countRating } = props;
     return(
-        <Card>
-            <img src={image}/>
-            <label>{title}</label>
-            <label>{category}</label>
-            <p>{description}</p>
-            <Rate allowHalf={allowHalfRate} defaultValue={rating}/>
-            <label>({countRating} reviews)</label>
-        </Card>
+
+        <div className={"cardComponent"}>
+            <img src={image} />
+            <Card>
+                <label>{title}</label>
+                <label>{category}</label>
+                <p>{description}</p>
+                <Rate allowHalf={allowHalfRate} defaultValue={rating}/>
+                <label>({countRating} reviews)</label>
+            </Card>
+        </div>
+
     )
 }
